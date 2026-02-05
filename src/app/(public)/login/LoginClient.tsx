7d89fcb9cc73920754f98c129d40687f46b93d0a
CoderwithUd <udayman178vi@gmail.com>
2026-02-05 12:44:05 +0530

    working in delpoy

--- src/app/(public)/login/LoginClient.tsx
+++ src/app/(public)/login/LoginClient.tsx
@@ -36,7 +36,6 @@ export function LoginClient() {
     resolver: zodResolver(schema),
     defaultValues: { email: "", password: "", rememberMe: true }
   });
-console.log("Testing login page")
   useEffect(() => {
     form.setFocus("email");
   }, [form]);
